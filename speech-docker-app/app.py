1e19a1e5f38fd9b375f572a80df5ab8aea8f5760
IlhanTaskin <ilhantaskin@std.iyte.edu.tr>
2026-04-29 19:25:03 +0300

    Prepare Docker setup for speech app

--- speech-docker-app/app.py
+++ speech-docker-app/app.py
@@ -88,4 +88,4 @@ def audio_file(filename):
     return send_from_directory(OUTPUT_FOLDER, filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
